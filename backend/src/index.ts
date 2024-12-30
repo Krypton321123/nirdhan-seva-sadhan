26abac29f3973ac40b02c98156595aa661ff54e8
Krypton321123 <coolrajsharma28@gmail.com>
2024-12-31 00:54:13 +0530

    vps hosting commit 3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,33 @@
 import dotenv from 'dotenv'
 import app from './app.js'
 import { connectMongo } from './db/connectMongo.js'; // just add the fucking .js in the import otherwise compiler goes shit
+import https from 'https'
+import fs from 'fs'
 
-dotenv.config(); 
+dotenv.config();
 
 const start = async () => {
-    try{
-        await connectMongo(); 
+    try {
+        await connectMongo();
+
+
+        const options = {
+            key: fs.readFileSync('/etc/letsencrypt/live/api.nirdhansewasansthan.com/privkey.pem'),
+            cert: fs.readFileSync('/etc/letsencrypt/live/api.nirdhansewasansthan.com/fullchain.pem')
+        };
+
         const port = parseInt(process.env.PORT as any) || 8000;
-        const displayURL = `http://localhost:${process.env.PORT}`
-        app.listen(port, '0.0.0.0',() => {
-            console.log(`App listening on ${displayURL}`)
-        })
 
-    } catch(err) {
-        console.log("Error starting the app" + err)
-        process.exit(1)
+
+        const displayURL = `https://api.nirdhansewasansthan.com:${port}`;
+        https.createServer(options, app).listen(port, '0.0.0.0', () => {
+            console.log(`App listening on ${displayURL}`);
+        });
+
+    } catch (err) {
+        console.log("Error starting the app" + err);
+        process.exit(1);
     }
-    
-}
-
+};
 
 start()
